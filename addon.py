10cfb091b910a1ada9ed442382845fe6b87f4786
Jakubicek222 <106910269+Jakubicek222@users.noreply.github.com>
2023-12-18 18:07:55 +0100

    vytvorenie casti 1-7

--- addon.py
+++ addon.py
@@ -21,4 +21,29 @@ li = xbmcgui.ListItem('1x3 - Strašidelné miesto')
 li.setProperty("IsPlayable", "true")
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
+url = 'http://jd.wz.cz/Tucniaky.z.Madagaskaru.Strasidelne.miesto.Sk.dub.avi'
+li = xbmcgui.ListItem('1x3 - Strašidelné miesto')
+li.setProperty("IsPlayable", "true")
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://jd.wz.cz/Tucniaky.z.Madagaskaru.Operacia.Plysiaci.Sk.dub.avi'
+li = xbmcgui.ListItem('1x4 - Operácia: Plyšiaci')
+li.setProperty("IsPlayable", "true")
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://jd.wz.cz/Tucniaky.z.Madagaskaru.Stastny.den.krala.Juliena.Sk.dub.avi'
+li = xbmcgui.ListItem('1x5 - Šťastný deň kráľa Juliena')
+li.setProperty("IsPlayable", "true")
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://jd.wz.cz/Tucniaky.z.Madagaskaru.Rodicovsky.instinkt.Sk.dub.avi'
+li = xbmcgui.ListItem('1x6 - Rodičovský inštinkt')
+li.setProperty("IsPlayable", "true")
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://jd.wz.cz/Tucniaky.z.Madagaskaru.Utok.a.baterky.Sk.dub.avi'
+li = xbmcgui.ListItem('1x7 - Útok a baterky')
+li.setProperty("IsPlayable", "true")
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
 xbmcplugin.endOfDirectory(addon_handle)
