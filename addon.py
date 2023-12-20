cd0d1df9ceb73e0bd819b4ba04b1498396832e44
Jakubicek222 <106910269+Jakubicek222@users.noreply.github.com>
2023-12-20 17:40:58 +0100

    pridanie casti 9 - 20, uprava kompatibility

--- addon.py
+++ addon.py
@@ -46,4 +46,69 @@ li = xbmcgui.ListItem('1x7 - Útok a baterky')
 li.setProperty("IsPlayable", "true")
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
+url = 'http://jd.wz.cz/Tucniaky.z.Madagaskaru.Tucniaky.beru.vsetko.Sk.dub.avi'
+li = xbmcgui.ListItem('1x8 - Tučniaky berú všetko')
+li.setProperty("IsPlayable", "true")
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://jd.wz.cz/Tucniaky.z.Madagaskaru.Zakon.o.zakaze.dotykania.sa.noh.Sk.dub.avi'
+li = xbmcgui.ListItem('1x9 - Zákon o zákaze dotýkania sa nôh')
+li.setProperty("IsPlayable", "true")
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://jd.wz.cz/Tucniaky.z.Madagaskaru.Nastrahy.netu.Sk.dub.avi'
+li = xbmcgui.ListItem('1x10 - Nástrahy netu')
+li.setProperty("IsPlayable", "true")
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://jd.wz.cz/Tucniaky.z.Madagaskaru.Korunovani.hlupaci.Sk.dub.avi'
+li = xbmcgui.ListItem('1x11 - Korunovaní hlupáci')
+li.setProperty("IsPlayable", "true")
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://jd.wz.cz/Tucniaky.z.Madagaskaru.V.utajeni.Sk.dub.avi'
+li = xbmcgui.ListItem('1x12 - V utajení')
+li.setProperty("IsPlayable", "true")
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://jd.wz.cz/Tucniaky.z.Madagaskaru.Nove.kralovstvo.Sk.dub.avi'
+li = xbmcgui.ListItem('1x13 - Nové kráľovstvo')
+li.setProperty("IsPlayable", "true")
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://jd.wz.cz/Tucniaky.z.Madagaskaru.Male.zoologicke.kupe.Sk.dub.avi'
+li = xbmcgui.ListItem('1x14 - Malé zoologické kupé')
+li.setProperty("IsPlayable", "true")
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://jd.wz.cz/Tucniaky.z.Madagaskaru.Vsetko.je.prehltnute.Sk.dub.avi'
+li = xbmcgui.ListItem('1x15 - Všetko je prehltnuté')
+li.setProperty("IsPlayable", "true")
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://jd.wz.cz/Tucniaky.z.Madagaskaru.Pukancova.panika.Sk.dub.avi'
+li = xbmcgui.ListItem('1x16 - Pukancová panika')
+li.setProperty("IsPlayable", "true")
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://jd.wz.cz/Tucniaky.z.Madagaskaru.Rybacka.Sk.dub.avi'
+li = xbmcgui.ListItem('1x17 - Rybačka')
+li.setProperty("IsPlayable", "true")
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://jd.wz.cz/Tucniaky.z.Madagaskaru.Zazrak.na.lade.Sk.dub.avi'
+li = xbmcgui.ListItem('1x18 - Zázrak na ľade')
+li.setProperty("IsPlayable", "true")
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://jd.wz.cz/Tucniaky.z.Madagaskaru.Strasidelna.ihla.Sk.dub.avi'
+li = xbmcgui.ListItem('1x19 - Strašidelná ihla')
+li.setProperty("IsPlayable", "true")
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://jd.wz.cz/Tucniaky.z.Madagaskaru.Zatmenie.Sk.dub.avi'
+li = xbmcgui.ListItem('1x20 - Zatmenie')
+li.setProperty("IsPlayable", "true")
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
 xbmcplugin.endOfDirectory(addon_handle)
